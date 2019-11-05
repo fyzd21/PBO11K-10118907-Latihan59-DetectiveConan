1d6206b496c5a6e8274e34f3ae9726473ac89d3e
unknown <fadlullahyazid@gmail.com>
2019-11-05 23:39:10 +0700

    First commit

---
+++ src/pbo11k/pkg10118907/latihan59/detectiveconan/OrangTua.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo11k.pkg10118907.latihan59.detectiveconan;
+
+/**
+ *
+ * @author programmer
+ */
+public class OrangTua {
+    public String bentukDasi() {
+        return "Dasi Biasa";
+    }
+}
---
+++ src/pbo11k/pkg10118907/latihan59/detectiveconan/PBO11K10118907Latihan59DetectiveConan.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo11k.pkg10118907.latihan59.detectiveconan;
+
+/**
+ *
+ * @author
+ * NAMA     : Yazid Fadlullah At-Taukily
+ * KELAS    : PBO11K
+ * NIM      : 10118907
+ * Deskripsi Program : Latihan menampilkan detective conan
+ * 
+ */
+public class PBO11K10118907Latihan59DetectiveConan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        OrangTua o = new OrangTua();
+        Anak a = new Anak();
+        
+        System.out.println("Bentuk dasi orang tua : "+o.bentukDasi());
+        System.out.println("Bentuk dasi anak : "+a.bentukDasi());
+    }
+    
+}
